dd37997ec6ee3430e62daee298bd352dd0174b7e
Huiting <huitingyeong75@gmail.com>
2023-08-20 12:03:05 +0800

    first

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = "AKIAULTTLJDIIIMJLHEG"
+  secret_key = "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"  
+}
