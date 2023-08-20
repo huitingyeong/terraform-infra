8498a904f2869bc9193d9a37543848f141e1fcb8
Huiting <huitingyeong75@gmail.com>
2023-08-20 19:28:46 +0800

    add

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-08fb9b8a78dafc662"
+  vpc_id      = "vpc-06d1b7cdf749baf0d"
   tags = {
     Name = "allow_tls"
   }
