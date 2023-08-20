08b76bec324722e3b9399ae6c02023453ae8ca44
Huiting <huitingyeong75@gmail.com>
2023-08-20 12:40:55 +0800

    add

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "fegesgsegrehrher"
-
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-08fb9b8a78dafc662"
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "allow_tls"
   }
 }
